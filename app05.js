876c741200c6b98e65dd7ab7a3922f4b3552901b
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-06-09 10:02:30 +0900

    sample05.html 追加

---
+++ app05.js
@@ -0,0 +1,9 @@
+var app = new Vue ({
+    el:'#app',
+    data:{
+        messages:[
+            {text:'This is normal message.',error:false},
+            {text:'aaaaaaaaaaaaaaaaaaaaaa.',error:true}
+        ]
+    }
+})
